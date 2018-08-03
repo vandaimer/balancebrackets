f35dd57874e54b61f58ff945f643a132e6174d49
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-03 01:07:12 -0300

    Fix eslint issues

--- src/BalanceBracket.js
+++ src/BalanceBracket.js
@@ -5,6 +5,32 @@ export const BRACKETS_RIGHT = '}';
 export const SQUARE_BRACKETS_LEFT = '[';
 export const SQUARE_BRACKETS_RIGHT = ']';
 
+export const simpleCheck = input => {
+  if (!input) return false;
+  // eslint-disable-next-line no-useless-escape
+  const pattern = /([\[\]\(\)\{\}])+/g;
+  const match = input.match(pattern);
+  if (!match) return false;
+  return match[0] === input;
+};
+
+export const check = (characterLeft, characterRight, input) => {
+  let counter = 0;
+  const characterList = input.split('');
+
+  for (let i = 0; i < characterList.length; i+=1) {
+    const value = characterList[i];
+    if (value === characterLeft) {
+      counter += 1;
+    } else if (value === characterRight) {
+      counter -= 1;
+    }
+    if (counter < 0) return false;
+  }
+
+  return counter === 0;
+};
+
 const balanceBrackets = input => {
   if (!simpleCheck(input)) return false;
 
@@ -15,29 +41,5 @@ const balanceBrackets = input => {
   return parenthere && bracket && squareBracket;
 };
 
-export const check = (characterLeft, characterRight, input) => {
-  let counter = 0;
-  const characterList = input.split('');
-
-  for (let i = 0; i < characterList.length; i++) {
-    const value = characterList[i];
-    if (value == characterLeft) {
-      counter++;
-    } else if (value == characterRight) {
-      counter--;
-    }
-    if (counter < 0) return false;
-  }
-
-  return counter == 0;
-};
-
-export const simpleCheck = input => {
-  if (!input) return false;
-  const pattern = /([\[\]\(\)\{\}])+/g;
-  const match = input.match(pattern);
-  if (!match) return false;
-  return match[0] === input;
-};
 
 export default balanceBrackets;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import BalanceBracket from './BalanceBracket';
 const [, , input] = process.argv;
 const isValidString = BalanceBracket(input);
 
+
+// eslint-disable-next-line no-console
 console.log('********************', ' RESULT ', '********************');
+// eslint-disable-next-line no-console
 console.log(`Is valid the string "${input}" ? Result: ${isValidString}`);
