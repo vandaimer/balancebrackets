50214216404c754a1a118e0cbdc73c93c9bfce19
Luiz Filipe <moresco.luiz@gmail.com>
2018-08-02 21:25:47 -0300

    Return a feedback if input is valid or not

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import BalanceBrackets from './BalanceBrackets';
 const [,,input] = process.argv;
 const isValidString = BalanceBrackets(input);
 
+console.log('********************', ' RESULT ', '********************');
+console.log(`Is valid the string "${input}" ? Result: ${isValidString}`);
